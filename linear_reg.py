28d271bf73d9b52d129cace7505951248e5c0955
Arwa Ahmed <arwaahmedmahmoud99@gmail.com>
2022-01-09 23:16:39 +0200

    first commit

---
+++ linear_reg.py
@@ -0,0 +1,56 @@
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+
+# Any results you write to the current directory are saved as output.
+
+#Import Libraries
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+np.set_printoptions(threshold=np.inf, linewidth=np.nan)
+!pwd
+
+#Importing DataSet 
+dataset = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/swe/kc_house_data.csv")
+space=dataset['sqft_living'] #space 
+price=dataset['price'] # vs price 
+
+x = np.array(space).reshape(-1, 1)
+y = np.array(price)
+# array.reshape(-1, 1) if your data has a single feature or array.reshape(1, -1) if it contains a single sample.
+#Splitting the data into Train and Test
+#from sklearn.cross_validation import train_test_split 
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=1/3, random_state=0) # divide your DATA into train and test
+
+
+#Fitting simple linear regression to the Training Set
+from sklearn.linear_model import LinearRegression 
+regressor = LinearRegression()
+regressor.fit(xtrain, ytrain)
+
+#Predicting the prices
+pred = regressor.predict(xtest)
+
+#Visualizing the training Test Results 
+plt.scatter(xtrain, ytrain, color= 'red')
+plt.plot(xtrain, regressor.predict(xtrain), color = 'blue')
+plt.title ("Visuals for Training Dataset")
+plt.xlabel("Space")
+plt.ylabel("Price")
+plt.show()
+
+#Visualizing the Test Results 
+plt.scatter(xtest, ytest, color= 'red')
+plt.plot(xtrain, regressor.predict(xtrain), color = 'blue')
+plt.title("Visuals for Test DataSet")
+plt.xlabel("Space")
+plt.ylabel("Price")
+plt.show()
